9681d503f82e1b09ab652c62a5215ca213ec09fc
joaogkm <joaogkm@gmail.com>
2025-03-09 09:54:34 -0300

    trocaUtilFiles

--- pages/01_🤖_Chat.py
+++ pages/01_🤖_Chat.py
@@ -2,8 +2,10 @@ import streamlit as st
 import os
 import openai
 import requests
+import re
+import pickle
 # from utils_openai import retorna_resposta_modelo
-from utils_files import *
+# from utils_files import *
 from dotenv import load_dotenv
 from fake_useragent import UserAgent
 from langchain_community.document_loaders import (
@@ -13,6 +15,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from time import sleep
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
+from pathlib import Path
+from unidecode import unidecode
 
 # Carrega arquivo .env
 load_dotenv('.env')
@@ -368,6 +372,74 @@ def obter_urls_sitemap(url_sitemap):
     except Exception as e:
         print(f"Erro ao carregar o Sitemap {url_sitemap}: {e}")
         return []
+
+
+# =================================================================
+# SALVAMENTO E LEITURA DE CONVERSAS
+# =================================================================
+
+
+PASTA_MENSAGENS = Path(__file__).parent / 'mensagens'
+PASTA_MENSAGENS.mkdir(exist_ok=True)
+CACHE_DESCONVERTE = {}
+
+
+def converte_nome_mensagem(nome_mensagem):
+    nome_arquivo = unidecode(nome_mensagem)
+    nome_arquivo = re.sub('\W+', '', nome_arquivo).lower()
+    return nome_arquivo
+
+
+def desconverte_nome_mensagem(nome_arquivo):
+    if not nome_arquivo in CACHE_DESCONVERTE:
+        nome_mensagem = ler_mensagem_por_nome_arquivo(
+            nome_arquivo, key='nome_mensagem')
+        CACHE_DESCONVERTE[nome_arquivo] = nome_mensagem
+    return CACHE_DESCONVERTE[nome_arquivo]
+
+
+def retorna_nome_da_mensagem(mensagens):
+    nome_mensagem = ''
+    for mensagem in mensagens:
+        if mensagem['role'] == 'user':
+            nome_mensagem = mensagem['content'][:30]
+            break
+    return nome_mensagem
+
+
+def salvar_mensagens(mensagens):
+    if len(mensagens) == 0:
+        return False
+    nome_mensagem = retorna_nome_da_mensagem(mensagens)
+    nome_arquivo = converte_nome_mensagem(nome_mensagem)
+    arquivo_salvar = {'nome_mensagem': nome_mensagem,
+                      'nome_arquivo': nome_arquivo,
+                      'mensagem': mensagens}
+    with open(PASTA_MENSAGENS / nome_arquivo, 'wb') as f:
+        pickle.dump(arquivo_salvar, f)
+
+
+def ler_mensagem_por_nome_arquivo(nome_arquivo, key='mensagem'):
+    with open(PASTA_MENSAGENS / nome_arquivo, 'rb') as f:
+        mensagens = pickle.load(f)
+    return mensagens[key]
+
+
+def ler_mensagens(mensagens, key='mensagem'):
+    if len(mensagens) == 0:
+        return []
+    nome_mensagem = retorna_nome_da_mensagem(mensagens)
+    nome_arquivo = converte_nome_mensagem(nome_mensagem)
+    with open(PASTA_MENSAGENS / nome_arquivo, 'rb') as f:
+        mensagens = pickle.load(f)
+    return mensagens[key]
+
+
+def listar_conversas():
+    conversas = list(PASTA_MENSAGENS.glob('*'))
+    conversas = sorted(
+        conversas, key=lambda item: item.stat().st_mtime_ns, reverse=True)
+    return [c.stem for c in conversas]
 
 
 # =================================================================
